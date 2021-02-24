4b469c24bc46a2bc84103f6552580ef4c71f432c
Grégory <milton-the-wizard@hotmail.fr>
2021-02-24 22:53:29 +0100

    Base backend

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const sauceRoutes = require('./routes/sauce');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://user:sMBguHcJs2ZeHhP@cluster0.uxdfw.mongodb.net/piquante?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/sauce', stuffRoutes);
+
+module.exports = app;
