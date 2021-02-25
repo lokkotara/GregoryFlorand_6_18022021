be5510ef062364579386d74c31ff1ee61ce84386
Grégory <milton-the-wizard@hotmail.fr>
2021-02-25 02:36:48 +0100

    Construction routeur + controllers

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+const { nextTick } = require('process');
 
-const app = express();
 
 mongoose.connect('mongodb+srv://user:sMBguHcJs2ZeHhP@cluster0.uxdfw.mongodb.net/piquante?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// app.use((req, res) => {
+//   res.json({ message: 'Votre requête a bien été reçue !' });
+// });
+const app = express();
+
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -21,6 +29,9 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use('/api/sauce', stuffRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,25 +1,56 @@
-const Sauce = require('../models/sauce');
+const Sauce = require('../models/Sauce');
+const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
-  delete req.body._id;
+  const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id;
   const sauce = new Sauce({
-    ...req.body
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [],
+    usersDisliked: []
   });
   sauce.save()
-    .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
-    .catch(error => res.status(400).json({ error }));
+  .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
+  .catch(error => res.status(400).json({ error }));
 };
 
 exports.modifySauce = (req, res, next) => {
-  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
-    .catch(error => res.status(400).json({ error }));
+  if (req.file) {
+    Sauce.findOne({_id: req.params.id})
+        .then(sauce => {
+            const fileName = sauce.imageUrl.split('/images/')[1]
+            fs.unlink(`images/${fileName}`, (err => {
+                if (err) console.log(err);
+                else {
+                    console.log("\nDeleted file: " + fileName);
+                }
+            }))
+        })
+  } 
+  const sauceObject = req.file ?
+  {
+    ...JSON.parse(req.body.sauce),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
+Sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
+  .then(() => res.status(200).json( 'message: Sauce modifiée !'))
+  .catch(error => res.status(404).json({ error }));
 }
 
 exports.deleteSauce = (req, res, next) => {
-  Sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
-    .catch(error => res.status(400).json({ error }));
+  Sauce.findOne({ _id: req.params.id})
+    .then(sauce => {
+      const filename = sauce.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Sauce.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
+          .catch(error => res.status(400).json({ error }));
+      })
+    })
+    .catch(error => res.status(500).json({ error }));
 }
 
 exports.getOneSauce = (req, res, next) => {
