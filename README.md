60bc6593b1da0bd6ce212ec0ee30c0225c614fed
Grégory <milton-the-wizard@hotmail.fr>
2021-03-10 10:04:35 +0100

    Fin des commentaires et ajout du readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-backend
+# Projet 6 - SoPekocko
+## Vue d'ensemble du projet
+L’entreprise SoPekocko souhaite développer une application d’évaluation de ses sauces piquantes, appelée “Piquante”.
+
+Même si l’application deviendra peut-être un magasin en ligne dans un futur proche, Sophie, la product owner de So Pekocko, a décidé que le MVP du projet sera une application web permettant aux utilisateurs d’ajouter leurs sauces préférées et de liker ou disliker les sauces ajoutées par les autres utilisateurs.
+
+## Que dois-je réaliser ?
+Je dois réaliser le côté backend de l'application ainsi que la création de l'API
+
+## Installation du front-end
+Pour installer la partie front-end du projet, veuillez cloner le repository disponible en suivant ce lien : https://github.com/OpenClassrooms-Student-Center/dwj-projet6. 
+
+Vous trouverez les instructions dans le README du repo.
+
+## Installation du back-end
+Après avoir cloné le repository, Il vous faudra installer les packages avec `npm install`.
+
+Vous devrez faire une copie du fichier `.env.example` que vous mettrez au même niveau (racine du backend). Il vous faudra ensuite le renommer en `.env` et remplir les informations de connexion nécessaires.
+
+Une fois terminé, vous pourrez lancer l'API en tapant `nodemon server` dans la console
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const http = require('http');
+const http = require('http');//Permet d'utiliser le serveur http
 const app = require('./app');
 
+//Permet de retourner un port valide, qu'il soit obtenu sous la forme du nombre ou d'une chaîne de caractères
 const normalizePort = val => {
   const port = parseInt(val, 10);
 
@@ -13,13 +14,14 @@ const normalizePort = val => {
   return false;
 };
 const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+app.set('port', port);//On indique quel port utiliser dans app.js
 
+//Permet de renvoyer les erreurs afin de les traiter correctement
 const errorHandler = error => {
   if (error.syscall !== 'listen') {
     throw error;
   }
-  const address = server.address();
+  const address = server.address();//Définit l'addresse de connexion au serveur
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
   switch (error.code) {
     case 'EACCES':
@@ -35,10 +37,10 @@ const errorHandler = error => {
   }
 };
 
-const server = http.createServer(app);
+const server = http.createServer(app);//Construit le serveur avec le protocole http et en utilisant app.js
 
-server.on('error', errorHandler);
-server.on('listening', () => {
+server.on('error', errorHandler);//Lance le gestionnaire d'erreurs
+server.on('listening', () => {//lance le serveur et affiche les infos de connexion dans la console
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
   console.log('Listening on ' + bind);
